5d1285fc8f139dcdbd686987e8f72fca3987db35
Sergei Dmitriev <Sergey.Dmitriev@mvideo.ru>
2026-05-18 15:52:24 +0300

    Добавит основную навигацию

--- src/app/layouts/app-layout/header/header.tsx
+++ src/app/layouts/app-layout/header/header.tsx
@@ -1,10 +1,12 @@
 import { Logo } from '../../../../shared/ui/logo';
+import MainNav from './main-nav/main-nav';
 
 function Header() {
   return (
     <header className="header" id="header">
       <div className="container">
         <Logo />
+        <MainNav className="header__main-nav" />
       </div>
     </header>
   );
